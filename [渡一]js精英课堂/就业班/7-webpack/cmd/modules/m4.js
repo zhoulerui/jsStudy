09088a607f6b2a4d7af2e6ff5c3900630dddd1bd
xing.org1^ <xing.org1@outlook.com>
2019-04-19 15:13:25 +0800

    webpack

--- [渡一]js精英课堂/就业班/7-webpack/cmd/modules/m4.js
+++ [渡一]js精英课堂/就业班/7-webpack/cmd/modules/m4.js
@@ -2,10 +2,10 @@
  * @Author: @Guojufeng 
  * @Date: 2019-04-18 18:05:32 
  * @Last Modified by: @Guojufeng
- * @Last Modified time: 2019-04-18 19:10:44
+ * @Last Modified time: 2019-04-18 19:20:45
  * m4模块
  */
-define(function(require,exports,module){
-  console.log('hhhhh')
-
+// 直接抛出去一个对象也是可以的
+define({
+  module: 'm4'
 });
