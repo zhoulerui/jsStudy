d0bec0126adca543a7b90bfa0d2595c502fd6f90
xing.org1^ <xing.org1@outlook.com>
2019-04-26 20:14:40 +0800

    哎

--- [渡一]js精英课堂/就业班/7-webpack/webpack-start/src/js/index.js
+++ [渡一]js精英课堂/就业班/7-webpack/webpack-start/src/js/index.js
@@ -1,4 +1,14 @@
+import { addFun } from './sum';
 import '../css/style.css';
 
 console.log('js');
 console.log('index.js');
+
+console.log('addFun函数调用',addFun(2,3));
+
+
+
+var a = document.createElement('a');
+a.href = 'https://www.baidu.com';
+a.innerText = '我是js动态填充进来的';
+document.body.appendChild(a);
--- [渡一]js精英课堂/就业班/7-webpack/webpack-start/webpack.config.js
+++ [渡一]js精英课堂/就业班/7-webpack/webpack-start/webpack.config.js
@@ -2,10 +2,15 @@
  * @Author: @Guojufeng 
  * @Date: 2019-04-25 19:58:12 
  * @Last Modified by: @Guojufeng
- * @Last Modified time: 2019-04-25 20:22:43
+ * @Last Modified time: 2019-04-26 20:13:36
  */
-var path = require('path');//引入node接口
+var path = require('path');//引入node接口 - 路径
+// var glob = require('glob');//glob变量
+var glob = require('glob-all');//glob变量
 var HtmlWebpackPlugin = require('html-webpack-plugin');//引入插件
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');//引入插件,用法见github,用于单独抽离css文件
+var PurifyCSSPlugin = require('purifycss-webpack');
+var WebpackDeepScopeAnalysisPlugin = require('webpack-deep-scope-plugin').default;//引入插件,用法见github
 var config = {
   mode: 'development',//开发环境，不压缩出口文件
   entry: {
@@ -18,15 +23,27 @@ var config = {
   module: {
     rules: [{
       test: /\.css$/,
-      use: ['style-loader','css-loader']//加载处理css的loader，因为除了js文件以外，webpack都要依赖loader才能处理文件
+      use: [MiniCssExtractPlugin.loader,'css-loader']//单独抽离css文件和解析的行间样式style-loader是不能在一起的。所以改成这样
+      // use: ['style-loader','css-loader']//加载处理css的loader，因为除了js文件以外，webpack都要依赖loader才能处理文件
     }]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].min.css'
+    }),
+    /* new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync([
+        path.join(__dirname, './*.html'),
+        path.join(__dirname, './src/js/*.js')//检测js中动态加的html结构和css样式，不过滤他们的代码
+      ])//匹配路径，所有根目录下src文件夹中以html结尾的。
+    }),/ /css tree shaking要放在js tree shaking的上边*/
     new HtmlWebpackPlugin({
       title: 'html-webpack-plugin生成title',//配置生成html的title
-      filename: 'app.html',//配置生成的html文件名字及目录，dist为根目录
-      template: './src/index.html'//配置生成的html以哪个html文件为模板，设置了以这个为模板后，title就不起作用了
-    })
+      filename: 'index.html',//配置生成的html文件名字及目录，dist为根目录
+      template: './index.html'//配置生成的html以哪个html文件为模板，设置了以这个为模板后，title就不起作用了
+    }),
+    new WebpackDeepScopeAnalysisPlugin(), // js-tree-shaking插件
   ]
 };
 module.exports = config;
