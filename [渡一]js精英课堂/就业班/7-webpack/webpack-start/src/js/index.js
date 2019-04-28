1223dceb6ea5f258cafa3c16ed7457f4a9a728f8
xing.org1^ <xing.org1@outlook.com>
2019-04-28 21:21:54 +0800

    webpack结业

--- [渡一]js精英课堂/就业班/7-webpack/webpack-start/src/js/index.js
+++ [渡一]js精英课堂/就业班/7-webpack/webpack-start/src/js/index.js
@@ -12,3 +12,8 @@ var a = document.createElement('a');
 a.href = 'https://www.baidu.com';
 a.innerText = '我是js动态填充进来的';
 document.body.appendChild(a);
+
+
+if(module.hot){// 如果node模块开启了热更新
+  module.hot.accept();//就支持热更新
+}
