f31075bc74557ac4a6f38ff491b14ebb3cb9a42c
xing.org1^ <xing.org1@outlook.com>
2019-05-04 23:12:45 +0800

    五一假期

--- Duying/EmploymentClass/9es6/a-babelInit/demo.js
+++ Duying/EmploymentClass/9es6/a-babelInit/demo.js
@@ -6,7 +6,36 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-// es7 - 扩展运算符
+/*
+ * @Author: @Guojufeng 
+ * @Date: 2019-05-04 16:37:16 
+ * @Last Modified by: @Guojufeng
+ * @Last Modified time: 2019-05-04 18:54:08
+ */
+// 解构赋值
+// babel默认不转换API
+// 比如Object.assign、promise这些
+// Object.assign(目标对象,克隆对象1，克隆对象2);
+// 会以浅克隆的方式，将后边对象的值复制到目标对象里边去。
+// import '@babel/polyfill';
+var assign1 = {
+  a: 1,
+  b: {
+    c: 2
+  }
+};
+var assign2 = {
+  aa: 1,
+  bb: {
+    cc: 2
+  }
+};
+var newObj = Object.assign({}, assign1, assign2);
+console.log(newObj);
+newObj.aa = 3;
+newObj.b.c = '浅拷贝';
+console.log(assign1, assign2, newObj); // es7 - 扩展运算符
+
 var obj = {
   name: 'xing.org1',
   age: 18,
@@ -23,7 +52,21 @@ var obj2 = _objectSpread({}, obj, {
 console.log(obj2);
 obj2.IDCard.address = 'HangZhou CHINA';
 console.log(obj.IDCard.address, obj2); // 浅拷贝
+// es6扩展运算符浅克隆
 
+var arrA = [{
+  a: 1,
+  b: 1
+}, 'b', {
+  a: 2,
+  b: 2
+}];
+var arrB = [].concat(arrA); //相当于[].concat(arrA);
+
+console.log(arrB);
+arrB[0].a = '浅克隆？';
+arrB[1] = '浅克隆！';
+console.log('arrA：', arrA, 'arrB', arrB);
 var oUl = document.getElementsByTagName('ul')[0],
     oLi = oUl.getElementsByTagName('li');
 /* for (var i = 0; i < oLi.length; i++) {
@@ -56,6 +99,20 @@ for (var i = 0; i < oLi.length; i++) {
   _loop2(i);
 }
 
+var aaa = [];
+
+var _loop3 = function _loop3(i) {
+  aaa[i] = function () {
+    console.log(i);
+  };
+};
+
+for (var i = 0; i < 10; i++) {
+  _loop3(i);
+}
+
+aaa[6](); // 6
+
 var a = 21;
 console.log(a);
 a = 2;
@@ -73,7 +130,8 @@ if (true) {
 
 console.log(zanshi);
 console.log(window.zanshi);
-{
+
+if (true) {
   var _test = 1;
   console.log(_test);
   {
@@ -84,6 +142,7 @@ console.log(window.zanshi);
     }
   }
 } // 扩展运算符
+
 
 var arg2 = [1, 2, 3, 4, 5];
 
--- Duying/EmploymentClass/9es6/a-babelInit/test.js
+++ Duying/EmploymentClass/9es6/a-babelInit/test.js
@@ -1,8 +1,41 @@
+/*
+ * @Author: @Guojufeng 
+ * @Date: 2019-05-04 16:37:16 
+ * @Last Modified by: @Guojufeng
+ * @Last Modified time: 2019-05-04 18:54:08
+ */
+// 解构赋值
+
+// babel默认不转换API
+// 比如Object.assign、promise这些
+
+// Object.assign(目标对象,克隆对象1，克隆对象2);
+// 会以浅克隆的方式，将后边对象的值复制到目标对象里边去。
+// import '@babel/polyfill';
+var assign1 = {
+  a: 1,
+  b: {
+    c: 2
+  }
+}
+var assign2 = {
+  aa: 1,
+  bb: {
+    cc: 2
+  }
+};
+
+var newObj = Object.assign({}, assign1, assign2);
+console.log(newObj);
+newObj.aa = 3;
+newObj.b.c = '浅拷贝';
+console.log(assign1, assign2, newObj);
+
 // es7 - 扩展运算符
 var obj = {
   name: 'xing.org1',
   age: 18,
-  IDCard:{
+  IDCard: {
     num: 123123812412,
     address: 'BeiJing CHINA'
   }
@@ -13,11 +46,36 @@ var obj2 = {
 }
 console.log(obj2);
 obj2.IDCard.address = 'HangZhou CHINA';
-console.log(obj.IDCard.address,obj2);// 浅拷贝
+console.log(obj.IDCard.address, obj2); // 浅拷贝
 
+// es6扩展运算符浅克隆
+
+var arrA = [{
+
+  a: 1,
+
+  b: 1
+
+}, 'b', {
+
+  a: 2,
+
+  b: 2
+
+}];
+
+var arrB = [...arrA]; //相当于[].concat(arrA);
+
+console.log(arrB)
+
+arrB[0].a = '浅克隆？';
+
+arrB[1] = '浅克隆！';
+
+console.log('arrA：', arrA, 'arrB', arrB);
 
 var oUl = document.getElementsByTagName('ul')[0],
-    oLi = oUl.getElementsByTagName('li');
+  oLi = oUl.getElementsByTagName('li');
 /* for (var i = 0; i < oLi.length; i++) {
   oLi[i].onclick = (function(j){
     return function(e){
@@ -26,23 +84,30 @@ var oUl = document.getElementsByTagName('ul')[0],
   })(i)
 } */
 for (let i = 0; i < oLi.length; i++) {
-  oLi[i].onclick = function(e){
-      console.log(i,e);
-    };
+  oLi[i].onclick = function (e) {
+    console.log(i, e);
+  };
 }
 
 let arr = [];
 
 for (let i = 0; i < oLi.length; i++) {
-  arr[i] = function(){
-      console.log(i);
-    };
+  arr[i] = function () {
+    console.log(i);
+  };
 }
+let aaa = [];
+for (let i = 0; i < 10; i++) {
+  aaa[i] = function () {
+    console.log(i);
+  };
+}
+aaa[6](); // 6
 
 
 let a = 21;
 console.log(a);
-a =  2;
+a = 2;
 const b = 13;
 // b = 3;
 console.log(b);
@@ -53,50 +118,48 @@ let tisheng = 'let和const没有变量声明提升';
 
 let zanshi = 'Temporal Dead Zone';
 
-if(true){
+if (true) {
   let zanshi = 'Temporal Dead Zone';
   console.log(zanshi);
 }
 
 console.log(zanshi);
-console.log(window.zanshi)
-{
-	let test = 1;
-	console.log(test);
-	{
-		console.log(test);
-		let test = 2;
-		{
-			let test = 3;
-		}
-	}
+console.log(window.zanshi);
+if (true) {
+  let test = 1;
+  console.log(test); {
+    console.log(test);
+    let test = 2; {
+      let test = 3;
+    }
+  }
 }
 // 扩展运算符
-var arg2 = [1,2,3,4,5];
+var arg2 = [1, 2, 3, 4, 5];
 
-console.log(...arg2);// 读，展开数组成散列的项
+console.log(...arg2); // 读，展开数组成散列的项
 
-function test(a,b,...args){
-  console.log(a,b,args);//写，把散列的项写入到一个数组中
+function test(a, b, ...args) {
+  console.log(a, b, args); //写，把散列的项写入到一个数组中
 }
-test(1,2,3,4,5);
+test(1, 2, 3, 4, 5);
 
-function getSum(...arr){
+function getSum(...arr) {
   // let arr = Array.prototype.slice.call(arguments);
-  return arr.reduce((pre,cur) => {
+  return arr.reduce((pre, cur) => {
     return pre += cur;
-  },0);
+  }, 0);
 }
-console.log(getSum(1,2,3,4,6));
+console.log(getSum(1, 2, 3, 4, 6));
 
 // 数组合并
 
 // 读操作 - 数组合并
 
-let arr1 = [1,2,3,4],
-    arr2 = [6,7,8,9],
-    newArr = [...arr1,...arr2];
+let arr1 = [1, 2, 3, 4],
+  arr2 = [6, 7, 8, 9],
+  newArr = [...arr1, ...arr2];
 console.log(newArr);
 // 相当于下边的写法：
-let concatArr = [].concat(arr1,arr2);
+let concatArr = [].concat(arr1, arr2);
 console.log(concatArr);
